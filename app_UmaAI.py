0de26ae735300d14e6fde34f7023277138e29a96
Shelley1767 <ooki0521@gmail.com>
2022-09-19 21:25:58 +0900

    16th commit

--- app_UmaAI.py
+++ app_UmaAI.py
@@ -8,10 +8,76 @@ from sklearn.preprocessing import LabelEncoder
 import os
 import math
 import pickle
+import pandas as pd
+
+@st.experimental_memo
+def detect(image, _model, _le, namelist):
+    image = Image.open(image)
+    image = np.array(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    cv2.imwrite('temp.jpg',image)
+    image = cv2.imread('temp.jpg')
+    height, width = image.shape[:2]
+    size = (width, height)
+        
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    classifier = cv2.CascadeClassifier('./lbpcascade_animeface/lbpcascade_animeface.xml')
+    faces = classifier.detectMultiScale(gray_image)
+      
+    for x,y,w,h in faces:
+        #顔の部分を四角で囲む
+        cv2.rectangle(image, (x,y), (x+w,y+h), color=(0,0,255), thickness=3)
+        #分類器モデルに入れるための処理
+        face_image = image[y:y+h, x:x+h]
+        face_image_resize = cv2.resize(face_image, (64, 64))
+        X = face_image_resize.astype('float32') / 255
+        #モデルの予測ラベルを取得
+        label = _model.predict(X.reshape(-1, 64, 64, 3))
+        label = np.argmax(label, axis=1)
+        label = _le.inverse_transform(label)[0]
+        #キャラクターカウンタを増加させる
+        target_id = namelist.index(label)
+        st.session_state['count{}'.format(target_id)] += 1
+
+        #元画像の顔の上(x, y)地点に予測ラベルを描画
+        cv2.putText(image, label, (x, y-30), cv2.FONT_HERSHEY_COMPLEX, width/500, (0, 0, 255), math.ceil(width/1000), cv2.LINE_AA)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    
+    return image
+
+def chara_counter(namelist):
+    for i, name in enumerate(namelist):
+        statename = 'count{}'.format(i)
+
+        if statename not in st.session_state:
+            st.session_state[statename] = 0
+
+        col_1st, col_2nd, col_3rd , col_4th= st.columns(4)
+        with col_1st:
+            st.write(name)
+
+        with col_3rd:
+            if st.button("+", key=i*2):
+                st.session_state[statename] += 1
+
+        with col_4th:
+            if st.button("-", key=i*2+1):
+                st.session_state[statename] -= 1
+
+        with col_2nd:
+            st.write(st.session_state[statename])
+
+def chara_counter_init(namelist):
+    for i, name in enumerate(namelist):
+        statename = 'count{}'.format(i)
+        st.session_state[statename] = 0
+
+
 
 def main():
 
     st.set_page_config(layout="wide")
+
     #タイトルの表示
     st.title("ウマ娘 顔認識アプリ")
 
@@ -35,40 +101,39 @@ def main():
 
     #保存済みのモデルをロード
     model = tf.keras.models.load_model('saved_model/my_model.h5')
-    
+    with open('label.pickle', 'rb') as web:
+        le = pickle.load(web)
+
+    #ネームリストのロード
+    namelist = pd.read_csv('umanames.csv',index_col=False, header=None)
+    namelist = list(namelist[0])
+
     #画像ファイルが読み込まれた後，顔認識を実行
     if image != None:
-        
-        image = Image.open(image)
-        image = np.array(image)
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        cv2.imwrite('temp.jpg',image)
-        image = cv2.imread('temp.jpg')
-        height, width = image.shape[:2]
-        size = (width, height)
-        
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        classifier = cv2.CascadeClassifier('./lbpcascade_animeface/lbpcascade_animeface.xml')
-        faces = classifier.detectMultiScale(gray_image)
-        
-        with open('label.pickle', 'rb') as web:
-            le = pickle.load(web)
-
-        for x,y,w,h in faces:
-            #顔の部分を四角で囲む
-            cv2.rectangle(image, (x,y), (x+w,y+h), color=(0,0,255), thickness=3)
-            #分類器モデルに入れるための処理
-            face_image = image[y:y+h, x:x+h]
-            face_image_resize = cv2.resize(face_image, (64, 64))
-            X = face_image_resize.astype('float32') / 255
-            #モデルの予測ラベルを取得
-            label = model.predict(X.reshape(-1, 64, 64, 3))
-            label = np.argmax(label, axis=1)
-            label = le.inverse_transform(label)[0]
-            #元画像の顔の上(x, y)地点に予測ラベルを描画
-            cv2.putText(image, label, (x, y-30), cv2.FONT_HERSHEY_COMPLEX, width/500, (0, 0, 255), math.ceil(width/1000), cv2.LINE_AA)
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        image = detect(image, model, le, namelist)
         st.image(image, use_column_width=True)
+
+    col_left, col_right= st.columns(2)
+    with col_left:
+            st.markdown("""
+            ### カウンタ
+            """)
+
+    with col_right:
+        if st.button("Clear"):
+            detect.clear()
+            chara_counter_init(namelist)
+
+    #カウンタの説明の表示
+    st.markdown(
+    '''
+        検出したウマ娘をカウントします。   
+        検出結果が間違っている場合はボタンを押すことで手動で修正が行えます。  
+    '''
+    )
+
+    #キャラクターカウンタの生成
+    chara_counter(namelist)
  
 
 
